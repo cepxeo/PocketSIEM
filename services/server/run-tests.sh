8f110489793184e49c2f4b9c372fbc4c90f8959f
cepxeo <cepxeo@gmail.com>
2022-11-29 13:19:13 +0100

    rules update

--- services/server/run-tests.sh
+++ services/server/run-tests.sh
@@ -1,4 +1,5 @@
-sudo rm -r /var/lib/docker/volumes/server_postgres_data_test
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 cd services/server
 docker-compose up
+sudo rm -r /var/lib/docker/volumes/server_postgres_data_test
+sudo mkdir -p /var/lib/docker/volumes/server_postgres_data_test/_data
